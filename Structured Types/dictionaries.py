963bb9074a867c92a6c6dd1d51b2dff75cc68ba5
Ekaterina Abramova <e7.abramova@gmail.com>
2017-10-04 20:07:45 +0100

    Add files via upload

---
+++ Structured Types/dictionaries.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+@author: Dr Ekaterina Abramova, 2017
+STRUCTURED TYPES.
+
+Type: dictionaries (not of sequence type).
+"""
+
+# -----------------------------------------------------------------------------
+# -------------------------- create a dictionary ------------------------------
+myDict1 = {}
+myDict2 = dict()
+
+# example dictionary (key is 'Jan', value is 1)
+months = {'Jan':1, 'Feb':2, 'Mar':3, 'Apr':4, 'May':5, 'June':6}
+
+# add entry 
+months['July'] = None
+# change entry 
+months['July'] = 7
+
+# another way of creating a dictionary functionality is with lists 
+L = [ ['Jan',1], ['Feb',2], ['Mar',3], ['Apr',4], ['May',5], ['June',6] ]
+def keySearch(L,key):
+    for elem in L:
+        if elem[0] == key:
+            return elem[1]
+    return None
+# function call
+ans = keySearch(L,'Apr') # 4
+
+# printing keys
+d = dict()
+d={'a':1, 'b':2, 'c':3}
+for key in 'abc':  
+    print(key)
+    temp = d[key]
+    print(temp)
